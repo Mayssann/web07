9405dcec35320d5bb0c3b74587b27dc7dcaeb982
Your NameMayssan <mayssanashkar5@gmail.com>
2024-08-12 15:50:52 +0300

    added a new endpoint /data

--- serv.js
+++ serv.js
@@ -5,5 +5,13 @@ const PORT = process.env.PORT || 3030 // process envirement port
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.get('/data', function (req, res) {
+    var data =[
+        {id:11 ,name:'mysn' },
+        {id:12 ,name:'ymn' },
+        {id:13 ,name:'jana' }
+    ]
+    res.send(data)
+  })
 
 app.listen(PORT)
